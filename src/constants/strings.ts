1a96a7679e8fc64c8cdb5f61194a4c4661ffdd08
Gaurav kumar <121500265+ARtoRiAs10@users.noreply.github.com>
2026-02-24 17:50:45 +0530

    Update social media links and email in Strings class

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,20 +1,27 @@
-export default class Strings {
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/ARtoRiAs10";
+  static telegramUsername = "ARtoRiAs10";
+
   static github = "GitHub";
   static githubLink = "https://github.com/ARtoRiAs10";
   static githubUsername = "ARtoRiAs10";
 
   static linkedIn = "LinkedIn";
   static linkedInLink = "https://www.linkedin.com/in/gaurav-kumar-96a16b232";
-  static linkedInUsername = "Gaurav Kumar";
+  static linkedInUsername = "gaurav-kumar";
 
   static instagram = "Instagram";
   static instagramLink = "https://www.instagram.com/ARtoRiAs10";
   static instagramUsername = "ARtoRiAs10";
 
+  static twitter = "Twitter";
+  static twitterLink = "https://www.twitter.com/ARtoRiAs10";
+  static twitterUsername = "ARtoRiAs10";
+
   static email = "Email";
-  // Added mailto: protocol for safety
-  static primaryEmailLink = "mailto:gauravkumar2002.@gmail.com";
-  static primaryEmail = "gauravkumar2002.@gmail.com";
+  static primaryEmailLink = "mailto:gauravkumar.dev@gmail.com";
+  static primaryEmail = "gauravkumar.dev@gmail.com";
 
   static buyMeACoffee = "BuyMeACoffee";
   static buyMeACoffeeLink = "https://buymeacoffee.com/ARtoRiAs10";
@@ -27,3 +34,5 @@ export default class Strings {
   static fullName = "Gaurav Kumar";
   static shortName = "ARtoRiAs10";
 }
+
+export default Strings;
